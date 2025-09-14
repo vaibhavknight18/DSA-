1889535bd37c0c6c5cabf4fd9d9a9cc4a8a0216d
vaibhavknight18 <vaibhavganigerv@gmail.com>
2025-09-14 13:03:34 +0530

    Create BasicCodes.py

---
+++ BasicCodes.py
@@ -0,0 +1,22 @@
+Fibonacci (7 terms):
+0 1 1 2 3 5 8 
+
+Palindrome Check:
+121: True
+123: False
+
+Factorial:
+5! = 120
+
+Reverse String:
+hello -> olleh
+
+Prime Check:
+29: True
+10: False
+
+Largest Element in List:
+9
+
+Sum of Digits:
+1234 -> 10
