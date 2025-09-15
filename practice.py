b6a3efc1f15f8db9e67c4f77b082d67b280a8713
vaibhavknight18 <vaibhavganigerv@gmail.com>
2025-09-15 21:33:27 +0530

    Update practice.py

--- practice.py
+++ practice.py
@@ -56,30 +56,17 @@ class Solution(object):
                     l = mid + 1
         return -1
     #4. Minimum in rotated sorted array:
-    class Solution(object):
-    def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
         l = 0
         r = len(nums)-1
-        while l <= r:
-            mid = (l+r) // 2
-            if nums[mid] == target:
-                return mid
-            if nums[l] <= nums[mid]:
-                if target > nums[mid] or nums[l] > target:
-                    l = mid+1
-                else:
-                    r = mid-1
+        while l < r:
+            mid = (l + r)//2
+            if nums[mid] > nums[r]:
+                l= mid+1
             else:
-                if target < nums[mid] or nums[r] < target:
-                    r = mid-1
-                else:
-                    l = mid + 1
-        return -1
+                r = mid
+        return nums[l]
     
 
 
